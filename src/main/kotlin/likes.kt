252f64ec21477fd82f1bf96f20f286a602be9118
Eugene Khomitskiy <mtl_87@list.ru>
2021-06-13 02:13:26 +0300

    add project

---
+++ src/main/kotlin/likes.kt
@@ -0,0 +1,6 @@
+fun main() {
+        println("Введите количество лайков:")
+        val likes = readLine()!!.toInt()
+        val result = if (likes % 10 == 1 && likes != 11) "человеку" else "людям"
+        println("Ваша запись понравилась $likes $result")
+}
