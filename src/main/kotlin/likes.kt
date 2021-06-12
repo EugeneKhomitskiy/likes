d8e67dbc3bcd5a39511c5578b84e62246a3d160e
Eugene Khomitskiy <mtl_87@list.ru>
2021-06-13 02:14:56 +0300

    add cycle "while"

--- src/main/kotlin/likes.kt
+++ src/main/kotlin/likes.kt
@@ -1,6 +1,8 @@
 fun main() {
+    while (true) {
         println("Введите количество лайков:")
         val likes = readLine()!!.toInt()
         val result = if (likes % 10 == 1 && likes != 11) "человеку" else "людям"
         println("Ваша запись понравилась $likes $result")
+    }
 }
